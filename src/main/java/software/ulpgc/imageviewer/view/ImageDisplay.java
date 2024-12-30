aa9ffc7ec71d270984b118e4c2fecdfc4b3313d3
ElenaArtiles <morartielen@gmail.com>
2024-12-30 23:59:08 +0000

    ImageDisplay methods implemented

--- src/main/java/software/ulpgc/imageviewer/view/ImageDisplay.java
+++ src/main/java/software/ulpgc/imageviewer/view/ImageDisplay.java
@@ -2,7 +2,10 @@ package software.ulpgc.imageviewer.view;
 
 import software.ulpgc.imageviewer.model.Image;
 
+import java.awt.*;
+
 public interface ImageDisplay {
     void show(Image image);
     Image image();
+    void paint(Graphics g);
 }
